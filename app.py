2dca57cd234886523224974c274292197e5cb7a0
oheyek <oheyek@proton.me>
2025-12-28 16:06:44 +0100

    feat: add homepage redirects

--- app.py
+++ app.py
@@ -1,11 +1,34 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, request, url_for
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello_world():
+@app.route("/", methods=["GET", "POST"])
+def main():
+    if request.method == "POST":
+        action = request.form["action"]
+        if action == "temperature":
+            return redirect(url_for("temperature"))
+        elif action == "length":
+            return redirect(url_for("length"))
+        elif action == "weight":
+            return redirect(url_for("weight"))
     return render_template("index.html")
+
+
+@app.route("/temperature")
+def temperature():
+    return "<p>temperature endpoint</p>"
+
+
+@app.route("/length")
+def length():
+    return "<p>length endpoint</p>"
+
+
+@app.route("/weight")
+def weight():
+    return "<p>weight endpoint</p>"
 
 
 if __name__ == "__main__":
